8ad175931e5b945a8d12caf6157c6a92f93deb2e
cristobalvega96 <crelvegalu@ittepic.edu.mx>
2018-12-12 13:39:50 -0700

    feat: I think everything its OK!

--- routes/routes.vehicles.js
+++ routes/routes.vehicles.js
@@ -2,12 +2,24 @@
 const express = require('express')
 const router = express.Router()
 const _vehicles = require('../models/vehicles')
-const mongoose = require('mongoose')
-const _ = require('lodash')
+const _ = require('underscore')
 
+// Create all the vehicles
+router.post('/init', (req, res) => {
+    _.each(_.range(1, 10001, 1), x => {
+        _vehicles.create({
+            name: 'vehicles-' + x,
+            description: 'Description of the vehicles ' + x,
+            doors: _.random(0,4),
+            kilometers_initial: _.random(1, 50), // Cuando el auto es nuevo, no tiene más de 50 kilometros
+            kilometers_recorded: _.random(1, 100), // Cuando el carro está seminuevo
+            kilometers_sealed: _.random(0, 1001) // Cuando el auto esta usado
+        })
+    })
+    res.send('Autos creados')
+})
 
-
-//Get method to get all the products created ever
+//Get method to get all the vehicles created ever
 router.get('/', (req, res) => {
     _vehicles.find()
         .then(vehicles => {
@@ -24,34 +36,34 @@ router.get('/', (req, res) => {
 */
 
 router.get('/map/tires', (req, res) => {
-    _product.find()
-        .then(products => {
-            products ? res.json(products.map(x => {
-                x.priceWithDiscount = x.price - (x.price * (1 / x.discount))
+    _vehicles.find()
+        .then(vehicles => {
+            vehicles ? res.json(vehicles.map(x => {
+                x.kilometers_sealed = x.kilometers_initial - (x.kilometers_initial * (1 / x.kilometers_recorded))
                 return x
             }
             )) :
-                res.send("No hay productos")
+                res.send("No hay Vehiculos por vender")
         })
 })
 
-router.get('/map/raise/:raise', (req, res) => {
-    _product.find()
-        .then(products => {
-            products ? res.json(products.map(x => {
-                x.priceWithRaise = x.price + (x.price * (1 / req.params.raise))
+router.get('/map/kilometers/:kilometers', (req, res) => {
+    _vehicles.find()
+        .then(vehicles => {
+            vehicles ? res.json(vehicles.map(x => {
+                x.kilometers_sealed = x.kilometers_initial + (x.kilometers_initial * (1 / req.params.kilometers))
                 return x
             }
             )) :
-                res.send("No hay productos")
+                res.send("No hay Vehiculos por vender")
         })
 })
 
-router.get('/filter/stock', (req, res) => {
-    _product.find()
-        .then(products => {
-            products ? res.json(products.filter(product => product.stock === 0)) :
-                res.send("No hay productos")
+router.get('/filter/doors', (req, res) => {
+    _vehicles.find()
+        .then(vehicles => {
+            vehicles ? res.json(vehicles.filter(vehicles => vehicles.doors === 0)) :
+                res.send("No hay Vehiculos por vender")
         })
 })
 
@@ -64,40 +76,27 @@ router.get('/filter/stock', (req, res) => {
 */
 
 router.get('/filter/name', (req, res) => {
-    _product.find()
-        .then(products => {
-            products ? res.json(products.filter(product => product.name.length > 6)) :
-                res.send("No hay productos")
+    _vehicles.find()
+        .then(vehicles => {
+            vehicles ? res.json(vehicles.filter(vehicles => vehicles.name.length > 6)) :
+                res.send("No hay Vehiculos por vender")
         })
 })
 
-router.get('/reduce/stock', (req, res) => {
-    _product.find()
-        .then(products => {
-            products ? res.json({ "products": products.map(product => product.stock).reduce((x, y) => x + y) }) :
-                res.send("No hay productos")
+router.get('/reduce/doors', (req, res) => {
+    _vehicles.find()
+        .then(vehicles => {
+            vehicles ? res.json({ "vehicles": vehicles.map(vehicles => vehicles.doors).reduce((x, y) => x + y) }) :
+                res.send("No hay Vehiculos por vender")
         })
 })
 
-router.get('/reduce/price', (req, res) => {
-    _product.find()
-        .then(products => {
-            products ? res.json({ "products": products.map(product => product.price).reduce((x, y) => x + y) }) :
-                res.send("No hay productos")
+router.get('/reduce/tires', (req, res) => {
+    _vehicles.find()
+        .then(vehicles => {
+            vehicles ? res.json({ "vehicles": vehicles.map(vehicles => vehicles.tires).reduce((x, y) => x + y) }) :
+                res.send("No hay Vehiculos por vender")
         })
-})
-
-router.post('/init', (req, res) => {
-    _.each(_.range(1, 10001, 1), x => {
-        _product.create({
-            name: 'Product-' + x,
-            description: 'Description of the product ' + x,
-            price: _.random(1, 1001, true),
-            discount: _.random(1, 41),
-            stock: _.random(0, 1001)
-        })
-    })
-    res.send('Initialized data')
 })
 
 module.exports = router
